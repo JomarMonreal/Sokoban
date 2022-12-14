cbdf96c2ed9a9514f6347b40bb4b62d9b52ca62d
JomarMonreal <jpmonreal2035@gmail.com>
2022-12-15 00:54:48 +0800

    added order randomizer

--- map_randomizer.py
+++ map_randomizer.py
@@ -3,5 +3,11 @@ import random
 
 #returns a random map from game_maps
 def generate():
-    maps=os.listdir("game_maps")
+    maps=os.listdir("game_maps_10x10")
     return maps[random.randint(0,len(maps)-1)]
+
+def randomize_order(map_list):
+    result_list=[]
+    while len(map_list)>0:
+        result_list.append(map_list.pop(random.randint(0,len(map_list)-1)))
+    return result_list
