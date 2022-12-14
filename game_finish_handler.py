325d3f6a53851738551a70225056d81d1aaf18ee
JomarMonreal <jpmonreal2035@gmail.com>
2022-12-15 00:56:48 +0800

    integrated changes in file saving

--- game_finish_handler.py
+++ game_finish_handler.py
@@ -1,8 +1,9 @@
 import time
 import game_object_types
+import game_file_handler
 
 #check if the user finished the game
-def has_finished_game(positions):
+def has_finished_level(positions):
     #initialization
     box_positions=[]
     trigger_positions=[]
@@ -29,16 +30,21 @@ def has_finished_game(positions):
     return is_finished
 
 #finishes the game
-def finish_game(start_time,saved_time_spent,highscore):
+def finish_game(start_time,saved_time_spent,maps_data_folder):
     #intialization
     has_saved=False
     has_continued=False
+    highscore=0
+    highscore = game_file_handler.load_highscore(maps_data_folder)
     
     #computes the score and save as new highscore when beaten previous record
     end_time=time.time()
     time_spent=round((end_time-start_time)+saved_time_spent,2)
     if highscore==0 or time_spent<highscore:
         highscore=time_spent
+        game_file_handler.save_highscore(highscore,maps_data_folder)
+        print("Congratulations! You hava a new highscore of " + str(time_spent) +" seconds.")
     
     print("You have solved the puzzle for " + str(time_spent) +" seconds.")
+    input("Press Enter to return back to main menu: ")
     return has_saved,has_continued,highscore
