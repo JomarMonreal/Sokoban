02b4482572175692f5064c24007d492a45c78e4c
JomarMonreal <jpmonreal2035@gmail.com>
2022-12-15 00:54:18 +0800

    added game_mode menu

--- menu.py
+++ menu.py
@@ -1,23 +1,36 @@
+import display_clearer
+import game_file_handler
+
 #displays the main menu screen to the terminal
-def display_main_menu(has_saved,highscore):
+def display_main_menu():
+    display_clearer.clear()
     print("="*30)
     print("SOKOBAN")
-    print("High Score: " + str(highscore) + " seconds")
-    if has_saved:
-        print("[1] Continue Last Save")
-        print("[2] Start New Game")
-    else:
-        print("[1] Start New Game")
+    print("[1] 10x10 Mode")
+    print("[2] 15x15 Mode")
     print("[0] Exit")
     user_input= int(input("Enter option: "))
     print("="*30)
     return user_input
 
+def display_game_mode_menu(game_mode_data):
+    display_clearer.clear()
+    print("="*30)
+    print("SOKOBAN")
+    print("High Score: " + str(game_file_handler.load_highscore(game_mode_data)) + " seconds")
+    if game_file_handler.load_game(game_mode_data)!=None:
+        print("[2] Continue Last Save")
+    print("[1] Start New Game")
+    print("[0] Back to Main Menu")
+    user_input= int(input("Enter option: "))
+    print("="*30)
+    return user_input
+
 #displays the options the user can take in gameplay
-def display_game_menu():
+def display_gameplay_menu():
     print("="*30)
     print("What's your move?")
-    print("[W] Up [S] Down [A] Left [D]Right")
+    print("[W] Up [S] Down [A] Left [D] Right")
     print("[R] Restart")
     print("[Z] Save")
     print("[X] Back to Main Menu")
